fae2e2b79a57b1fc9ba7be5cd5c8eb35dbd6f146
lescobar <lescobar@umich.edu>
2021-03-06 22:24:48 -0500

    added playlistnames and names to songs databases

--- musicManiaV0/components/PlaylistEditorScreenWrapper.js
+++ musicManiaV0/components/PlaylistEditorScreenWrapper.js
@@ -1,21 +1,41 @@
 import * as React from 'react';
 import { useState } from 'react';
 import { Component } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, AsyncStorage } from 'react-native';
 import SelectOption from './SelectOption';
 
 export default class PlaylistEditorScreenWrapper extends Component {
+  state = {
+    playlist_names: [],
+  }
+
+  async getName() {
+    try{
+      let playlistsTemp = await AsyncStorage.getItem("Playlist_names");
+      this.setState({
+        playlist_names: JSON.parse(playlistsTemp),
+      });
+    }
+    catch(err){
+      alert(err);
+    }
+
+  }
+
 
   render(){
+    this.playlist_names = this.state.playlist_names.map((item) => 
+        <SelectOption name={item}/>
+    );
     return (
       <View style={styles.container}>
+        <SelectOption name="Get playlists" onPress={() => this.getName()}/>
         <Text style={styles.paragraph}>
           Tap the songs below to add them to the current playlist!
         </Text>
         <View style={styles.row}>
+          {this.playlist_names}
           <SelectOption name='My Favorites' onPress={() => this.props.nav.navigate('Playlist')}/>
-          <SelectOption name='Playlist 2' onPress={() =>  this.props.nav.navigate('Playlist')}/>
-          <SelectOption name='Playlist 3' onPress={() =>  this.props.nav.navigate('Playlist')}/>
           <SelectOption name='Create New Playlist' onPress={() =>  this.props.nav.navigate('Playlist')}/>
         </View>
       </View>
--- musicManiaV0/components/SelectScreenWrapper.js
+++ musicManiaV0/components/SelectScreenWrapper.js
@@ -1,12 +1,39 @@
 import * as React from 'react';
 import { useState } from 'react';
 import { Component } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, AsyncStorage } from 'react-native';
 import SelectOption from './SelectOption';
+import { TextInput } from 'react-native-gesture-handler';
 
 export default class SelectScreenWrapper extends Component {
   state = {
     playlist: [],
+    playlistName: '',
+  }
+  setName(playlistname){
+    this.state.playlistName = playlistname;
+  }
+  // save playlist
+  async savePlaylist() {
+    try{
+      //await AsyncStorage.clear();
+      let playlistsTemp = await AsyncStorage.getItem("Playlist_names");
+      if(playlistsTemp == null){
+        let playlists = [];
+        playlists.push(this.state.playlistName.toString());
+        await AsyncStorage.setItem("Playlist_names", JSON.stringify(playlists));
+      }
+      else{
+        let playlists = JSON.parse(playlistsTemp);
+        playlists.push(this.state.playlistName.toString());
+        await AsyncStorage.setItem("Playlist_names", JSON.stringify(playlists));
+      }
+      await AsyncStorage.setItem(this.state.playlistName.toString(), JSON.stringify(this.state.playlist));
+      //await AsyncStorage.clear();
+    }
+    catch (err){
+      alert(err);
+    }
   }
 
   addToPlaylist(name) {
@@ -35,6 +62,10 @@ export default class SelectScreenWrapper extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.paragraph}>
+          Name Playlist:
+        </Text>
+        <TextInput style={styles.input} onChangeText={(text) => this.setName(text)}/>
+        <Text style={styles.paragraph}>
           Tap the songs below to add them to the current playlist!
         </Text>
         <View style={styles.row}>
@@ -60,6 +91,9 @@ export default class SelectScreenWrapper extends Component {
         <View style={styles.playlist}>
           <Text style={styles.paragraph}>Playlist</Text>
           <View style={styles.playlist}>{this.playlist}</View>
+        </View>
+        <View>
+          <SelectOption name='Save Playlist!' onPress={() => this.savePlaylist()}/>
         </View>
       </View>
     )
@@ -98,5 +132,10 @@ const styles = StyleSheet.create({
   	fontSize: 20,
   	textAlign: 'center',
     color: 'black'
+  },
+  input: {
+    borderColor: 'white',
+    alignSelf:"stretch",
+    backgroundColor: 'white'
   }
 });
