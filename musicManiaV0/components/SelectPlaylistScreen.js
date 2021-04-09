d979f364f2687b2a09f03264d78557635a0a617f
houghj <houghj@umich.edu>
2021-04-09 19:22:54 -0400

    Fixed the crashing issue with the timer select!

--- musicManiaV0/components/SelectPlaylistScreen.js
+++ musicManiaV0/components/SelectPlaylistScreen.js
@@ -9,23 +9,29 @@ export default class SelectPlaylistScreen extends Component {
   constructor(props){
     super(props);
     this.componentRef = React.createRef();
-    this.makeTimer()
+    this.componentRef2 = React.createRef();
+    this.intervalID = 0;
   }
 
   state = {
     playlist_names: [],
     number:0,
+    current_selection: 0,
   }
 
   referComponentByRef = () => {
     this.componentRef.current.highlight();
+    this.componentRef2.current.highlight();
   }
 
   makeTimer(){
-    setInterval(() => {
-      let rand = Math.floor(Math.random() * 10) + 1
-        this.setState({number: rand})
-        this.componentRef.current.highlight();
+    this.intervalID = setInterval(() => {
+        this.setState({current_selection: this.state.current_selection+1})
+        console.log(this.componentRef);
+        if (this.componentRef.current != null){
+          this.componentRef.current.highlight();
+          this.componentRef2.current.highlight();
+        }
         // this.highlightElement((this.current_selection + 1)%this.playlist_names.length);
         // this.setState({
         //   current_selection : (this.current_selection + 1)%this.playlist_names.length
@@ -72,6 +78,11 @@ export default class SelectPlaylistScreen extends Component {
   componentDidMount(){
     this.getName();
     this.forceUpdate();
+    this.makeTimer();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
   }
 
   render(){
@@ -87,10 +98,10 @@ export default class SelectPlaylistScreen extends Component {
         <Theme.View>
           {this.playlist_names}
           <SelectOption name='Create New Playlist' onPress={() =>  this.newPlaylist('Playlist')}/>
-          <SelectOption ref={node => this.componentRef.current = node} name='Tester' onPress={() =>  this.newPlaylist('Playlist')}/>
-          {/* <SelectOption name='Change Color' onPress={this.referComponentByRef}/> */}
+          <SelectOption ref={node => this.componentRef.current = node} name='Tester' />
+          <SelectOption ref={node => this.componentRef2.current = node} name='Tester2' />
           <Theme.Text style={CustomStyleSheet.styles.baseParagraph}>
-          {this.state.number}
+          {this.state.current_selection}
           </Theme.Text>
         </Theme.View>
       </Theme.View>
